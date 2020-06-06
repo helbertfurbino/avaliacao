9387213259adb7e58a28ca9fbab8ef8ccc1000e3
helbertsamuel <helbertsamuel@gmail.com>
2020-06-06 15:33:39 -0300

    primeiro commit

--- questao1.php
+++ questao1.php
@@ -1,15 +1,15 @@
 <?php
 
-$estados = ['ES', 'MG', 'RJ', 'SP'];
-$cidades = array_reverse(['São Paulo', 'Rio de Janeiro', 'Minas Gerais', 'Espírito Santo']);
+$estadosSiglas = ['ES', 'MG', 'RJ', 'SP'];
+$estadoNomes = array_reverse(['São Paulo', 'Rio de Janeiro', 'Minas Gerais', 'Espírito Santo']);
 
 $capitais = [];
 
-for ($i = 0; $i < count($estados); $i++){
-    $capitais[$i] = [$estados[$i] => $cidades[$i]];
+for ($i = 0; $i < count($estadosSiglas); $i++){
+    $capitais[$i] = [$estadosSiglas[$i] => $estadoNomes[$i]];
 }
 
 
 for ($i = 0; $i < count($capitais); $i++){
-    echo key($capitais[$i]) . "-" . $capitais[$i][key($capitais[$i])] . "<br>";
+    echo key($capitais[$i]) . " - " . $capitais[$i][key($capitais[$i])] . "<br>";
 }
